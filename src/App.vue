48aa3e6447889a461f4fc03f1df99978272b88b6
Nick Westbrook <ntw3001@gmail.com>
2025-06-21 23:47:36 +0900

    just the tiniest bit of practice with attributes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
   import Hello from './components/hellow_Orld.vue';
+  import myComponent from './components/myComponent.vue';
+
+  const message = 'Hello World';
+
+  function calculate(x: number, y: number): number {
+    return x + y;
+  }
 </script>
 
 <template>
   <Hello/>
-  <Hello/>
-  <Hello/>
-  <Hello/>
-  <Hello/>
+  <h1>Looka me I'm a {{message}}</h1>
+  <div>A number is {{ 4 * 4 }}</div>
+  <div>A calculation is {{ calculate(5, 6) }}</div>
+  <myComponent/>
 </template>
 
 <style scoped>
---
+++ src/components/myComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  const mySite = 'https://www.example.com';
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to My Site</h1>
+    <p>Visit us at: <a :href="mySite" target="_blank">{{ mySite }}</a></p>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    color: #42b983;
+  }
+
+  p {
+    font-size: 16px;
+  }
+
+  a {
+    color: #42b983;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
