748d2a4114cfb8c64f7e93fd6d9ed2151f4394df
Nick Westbrook <ntw3001@gmail.com>
2025-06-21 23:59:53 +0900

    just the tiniest bit of practice with attributes

--- src/components/myComponent.vue
+++ src/components/myComponent.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
   const mySite = 'https://www.example.com';
+  const imgWidth = 400;
+  const imgInfo = {
+    alt: "IANA Logo",
+    src: "https://www.iana.org/_img/2025.01/iana-logo-header.svg"
+  };
+
 </script>
 
 <template>
   <div>
     <h1>Welcome to My Site</h1>
     <p>Visit us at: <a :href="mySite" target="_blank">{{ mySite }}</a></p>
+    <img v-bind:="imgInfo" :width="imgWidth" /> // we can bind an object full of sttributes qwith v-bind or just throw the : in front to bind a specific constant
   </div>
 </template>
 
